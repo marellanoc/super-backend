e85e321d5f0755cb60398b080463d18a78a1e01e
DecmanMedia <diego.martinez.12@sansano.usm.cl>
2019-01-07 15:12:18 -0300

    change Supermarket model

--- models/supermarket.model.js
+++ models/supermarket.model.js
@@ -3,8 +3,11 @@ var Schema = mongoose.Schema
 
 var supermarketShema = new Schema({
   name: String,
-  address: String,
-  logo: String,
+  picture: String,
+  branches: [{
+    address: String,
+    location: String,
+  }]
 });
 
 module.exports = mongoose.model('Supermarket', supermarketShema);
